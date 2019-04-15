58cf03127550de5dd9786f02a60123ff914c4650
unknown <kevin.cheung.kai@gmail.com>
2019-04-15 17:17:19 -0400

    connected mongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,6 +9,28 @@ app.use(expres.static('public'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+const Schema = mongoose.Schema
+
+let Todo = new Schema({
+  todo_description: {
+    type: String
+  },
+  todo_responsible: {
+    type: String
+  },
+  todo_priority: {
+    type: String
+  },
+  todo_completed: {
+    type: Boolean
+  }
+})
+
+module.exports = mongoose.model('Todo', Todo);
 
 app.listen(3000, function() {
   console.log('Server started on port 3000');
